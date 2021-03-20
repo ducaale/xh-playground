78a668a760d3e555b8389afc46400601449442c2
ducaale <sharaf.13@hotmail.com>
2021-03-20 00:27:02 +0000

    add comment about stdout, stdin and stderr

--- www/main.js
+++ www/main.js
@@ -1,4 +1,5 @@
 import WasmTerminal, { fetchCommandFromWAPM } from '@wasmer/wasm-terminal';
+import { WasmFs } from "@wasmer/wasmfs";
 import xh from '../Cargo.toml';
 import 'xterm/css/xterm.css';
 import './style.css';
@@ -6,9 +7,20 @@ import './style.css';
 const fetchCommandHandler = async ({args}) => {
   let commandName = args[0];
   if (commandName === 'xh') {
-    return async (...x) => {
+    return async (options, wasmFs) => {
+      // TODO: call these functions from xh side
+
+      // read from stdin
+      // let myArr = new Uint8Array(1024);
+      // let stdin = wasmFs.fs.readSync(0, myArr, 0, 1024, 0);
+
+      // output to stdout
+      // wasmFs.fs.writeFileSync("/dev/stdout", "Quick Start!");
+
+      // output to stderr
+      // wasmFs.fs.writeFileSync("/dev/stderr", "Quick Start!");
+
       const mod = await xh();
-      console.log(x);
       return mod.run();
     }
   }
@@ -17,7 +29,8 @@ const fetchCommandHandler = async ({args}) => {
 };
 
 const wasmTerminal = new WasmTerminal({
-  fetchCommand: fetchCommandHandler
+  fetchCommand: fetchCommandHandler,
+  wasmFs: new WasmFs()
 });
 
 wasmTerminal.print('Hello World!');
